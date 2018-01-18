ba0eea3b5afd05be5fc8203b335f26521a4f6066
Я <rudolf.z2013@gmail.com>
2018-01-18 17:23:34 +0200

    Initial Commit

---
+++ CalcHeath/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CalcHeath
+//
+//  Created by Rudcool on 18.01.2018.
+//  Copyright © 2018 rudcool inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
